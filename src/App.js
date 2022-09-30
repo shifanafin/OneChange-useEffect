fc5bec5cfc5f24150220e3f72df074e147e3d159
SHIFANA CT <ctshifanachennara@gmail.com>
2022-09-30 13:50:01 +0530

    onchange

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 function App() {
+  const [valueText,setvalue] =useState("")
+  const [name,setName] =useState("")
+
+
+  useEffect(()=>{
+             setName(valueText)
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+    <form>
+        <input
+          type="text"
+          value={valueText}
+          onChange={(e) => setvalue(e.target.value)}
+          placeholder="Your Name"
+        />
+        <p>{name}</p>
+      </form>
+</div>
+  
+  )
+  }
 
 export default App;
